81f6a9ec56f40b35391f6f04c79376b6f90b8395
GinnaBonilla2406 <g2406b@gmail.com>
2017-03-30 16:43:44 -0500

    nuevo.js

---
+++ js/nuevo.js
@@ -0,0 +1,71 @@
+//Autores: Ginna Bonilla - Jhon Moreno
+
+var acumuladorApp = angular.module( 'acumuladorApp', [] );      
+    acumuladorApp.controller( "acumuladorAppCtrl",
+        
+        //[ "$scope",  //Originalmente solo se minificaba el scope.
+        [ "$scope", "$http", //Esto es para minificar, pero interfiere con lo de php, hay que minificar las otras variables.
+         
+            function( $scope, $http )
+            {
+                $scope.cargar_datos_php = function()
+                {
+                   var lista= document.getElementById('datos');
+                    console.log(lista.length );
+                    var sintomas="";
+
+                    for (var i = 0 ; i < lista.length ; i++) 
+                    {
+                        if (lista.item(i).selected ) 
+                        {
+                            if (sintomas == ""){ 
+                                sintomas +=lista.item(i).value;
+                            }else{
+                                sintomas +="," +lista.item(i).value;
+                            }
+                        }
+                    }
+                    var cad2 = sintomas;
+                    console.log(cad2);
+                
+                    if( cad2.length > 0 )
+                    {
+                        console.log("Cadena" + cad2);
+                        //Aquí se hace el llamado a un php con conexión a MySQL.
+                        $http.get( 'llamado-php.php?cadena=' + cad2 ).success
+                        (
+                            function( response ) 
+                            { 
+                                console.log( response );
+                                $scope.campos = response.records;            
+                            }
+                        );   
+                    }                   
+                }
+
+
+                $scope.buscar = function()
+                {
+                    var busqueda = $scope.text_busqueda;    
+                    console.log(busqueda);
+                    //Aquí se hace el llamado a un php con conexión a MySQL.
+                     $http.get( 'llamado-php.php?busqueda=' + busqueda ).success
+                    (
+                        function( response ) 
+                        { 
+                            console.log( response );
+                            $scope.campos = response.records;            
+                        }
+                    );                                      
+                }
+                $scope.ocultar=function()
+                {
+                    console.log("Ginna la más consona");
+                    document.getElementById('mostrar').style.display="block";
+                    document.getElementById('ocultar').style.display="none";
+                }
+            }
+        ] //Si se minifica, se deben minificar todas las llamadas inyecciones, de lo contrario mejor no minifique nada.
+    );
+
+    
